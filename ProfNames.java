082e897c0f9f9ff1781409284479a8d923bb3f5d
Jonathan Hsing <>
2021-04-28 15:07:10 +0000

    updated ProfNames

--- ProfNames.java
+++ ProfNames.java
@@ -2,8 +2,15 @@ public class ProfNames
 {
   public int countNames(String[][]nameArray, String lastName)
   {
-
-
-    return 0;
+    int c = 0;
+    int l = lastName.length();
+    for(int a = 0; a < nameArray.length; a++){
+      for(int b = 0; b < nameArray[a].length; b++){
+        if(nameArray[a][b].substring(0,l).equals(lastName)){
+          c++;
+        }
+      }
+    }
+    return c;
   }
 }
